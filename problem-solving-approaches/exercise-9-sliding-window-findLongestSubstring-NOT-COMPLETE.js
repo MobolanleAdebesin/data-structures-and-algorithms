d978546161bedc1794df12b6bac388a450c25eb6
Mobolanle Adebesin <bolaadebesin@gmail.com>
2023-01-25 12:52:17 -0500

    add solution for find longest substring

--- problem-solving-approaches/exercise-9-sliding-window-findLongestSubstring-NOT-COMPLETE.js
+++ problem-solving-approaches/exercise-9-sliding-window-findLongestSubstring-NOT-COMPLETE.js
@@ -20,6 +20,15 @@ findLongestSubstring('longestsubstring') // 8
 findLongestSubstring('thisishowwedoit') // 6
 
 Break It Down 
+1.Write a function that accepts a string 
+2. Create a pointer p1 to track position in string, start at zero
+3. Create a variable, max, to keep track of the maximum substring, start at zero
+4. Create an object, dict, to keep track of the letters in the string 
+5. Loop through the string and while p1 is less than the length of the string: 
+    5b. if str[p1] is not in dict, add it to dict. 
+    5c. if the length of dict is greater than the current max, set max equal to the current length of dict 
+    5d. if str[p1] is in dict, clear dict, add str[p1] to dict. 
+6. At the end of the while loop return max. 
 
 Simplify / Solve 
 
@@ -30,20 +39,27 @@ Look back and refactor
 
 */
  
-findLongestSubstring('') // 0
-findLongestSubstring('rithmschool') // 7
-findLongestSubstring('thisisawesome') // 6
-findLongestSubstring('thecatinthehat') // 7
-findLongestSubstring('bbbbbb') // 1
-findLongestSubstring('longestsubstring') // 8
-findLongestSubstring('thisishowwedoit') // 6
 // Time Complexity - O(n)
 
 function findLongestSubstring(str){
     let max = 0
+    let p1 = 0; 
     if(!str || !str.length) return max 
     max+= 1; 
+    let dict = {}; 
+    while(p1 < str.length){
+        if(dict[str[p1]]){
+            p1 = dict[str[p1]] + 1; 
+            dict = {}
+        }
+        dict[str[p1]] = p1
+        if(Object.keys(dict).length > max){
+            max = Object.keys(dict).length
+        }
+        p1++; 
+    }
+
     return max
 }
-
+findLongestSubstring("longestsubstring")
 module.exports = findLongestSubstring; 
